c715fca1a11abe42dfade31d2d7378cbbf48cf07
Ubuntu <ubuntu@ip-172-31-28-80.ec2.internal>
2025-04-03 07:31:45 +0000

    node health

---
+++ nodehealth.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+###############################
+#
+#Author: Vinay Vardhan
+#Date: April 1st 2025
+#
+#This scripts outputs the nodehealth
+#
+###############################
+#
+
+set -x # debug the code
+set -e # exit the script when there is an error
+set -o # pipefail
+
+#echo "Disk space"
+df -h
+
+#echo "memory"
+free -g
+
+#echo"CPU"
+nproc
+
+ps -ef | grep amazon | awk F" " '{print $2}' # list the running process
