0785b4dfe804742e185a74c6d7e23bdd045805cc
iam-kvvardhan <vinaykomaragiri9@gmail.com>
2025-04-05 18:55:11 +0000

    Add nodehealth.sh script

--- nodehealth.sh
+++ nodehealth.sh
@@ -14,17 +14,15 @@ set -x # debug the code
 set -e # exit the script when there is an error
 set -o # pipefail
 
-********Disk Space*********
 #echo "Disk space"
 df -h
 
-********Memory*********
 #echo "memory"
 free -g
 
-********CPU*********
+
 #echo"CPU"
 nproc
 
-********Process*********
+
 ps -ef | grep amazon | awk F" " '{print $2}' # list the running process
