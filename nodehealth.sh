0eb985e722aafa3378a5c4aba0b269596203dce6
root <root@ip-172-31-28-80.ec2.internal>
2025-04-05 18:48:28 +0000

    Add nodehealth.sh script

--- nodehealth.sh
+++ nodehealth.sh
@@ -14,13 +14,17 @@ set -x # debug the code
 set -e # exit the script when there is an error
 set -o # pipefail
 
+********Disk Space*********
 #echo "Disk space"
 df -h
 
+********Memory*********
 #echo "memory"
 free -g
 
+********CPU*********
 #echo"CPU"
 nproc
 
+********Process*********
 ps -ef | grep amazon | awk F" " '{print $2}' # list the running process
